4b15ff7e64af01afc98122b99d6a7242c4723552
Matthew Lehner <matthew@mpl.io>
2024-01-20 16:33:38 -0800

    Add and style homepage content

--- lib/home_web/controllers/page_controller.ex
+++ lib/home_web/controllers/page_controller.ex
@@ -4,6 +4,8 @@ defmodule HomeWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> assign(:page_title, "Software developer, technical founder")
+    |> render(:home)
   end
 end
--- test/home_web/controllers/page_controller_test.exs
+++ test/home_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule HomeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+
+    assert html_response(conn, 200) =~
+             "Software developer, technical founder, and people focused leader"
   end
 end
