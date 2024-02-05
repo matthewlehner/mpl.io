ccfb27bb5c27a3b6cf8b3d6da6d8ea77e9d6f9f6
Matthew Lehner <matthew@mpl.io>
2024-02-04 18:08:23 -0800

    Make the writing index page look good

--- lib/home_web/controllers/article_html.ex
+++ lib/home_web/controllers/article_html.ex
@@ -2,4 +2,18 @@ defmodule HomeWeb.ArticleHTML do
   use HomeWeb, :html
 
   embed_templates "article_html/*"
+
+
+  attr :date, Date, required: true
+  attr :rest, :global
+
+  def published_at(assigns) do
+    ~H"""
+    <time {@rest} datetime={@date}><%= format_date(@date) %></time>
+    """
+  end
+
+  defp format_date(date) do
+    Calendar.strftime(date, "%B %-d, %Y")
+  end
 end
