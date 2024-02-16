5fb2b06759b88669443772d8d326b71b59fbf829
Matthew Lehner <matthew@mpl.io>
2024-02-16 15:36:47 -0800

    Remove unecessary calls to IO.inspect

--- lib/home/blog/article.ex
+++ lib/home/blog/article.ex
@@ -8,11 +8,9 @@ defmodule Home.Blog.Article do
       |> Path.rootname()
       |> Path.split()
       |> Enum.take(-1)
-      |> IO.inspect()
 
     [year, month, day, id] =
       String.split(month_day_id, "-", parts: 4)
-      |> IO.inspect()
 
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
     struct(__MODULE__, Keyword.merge(Map.to_list(attrs), id: id, date: date, body: body))
