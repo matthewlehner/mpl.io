6c3b414df603cb95c479a3f96806ca0a9d4ad9d6
Matthew Lehner <matthew@mpl.io>
2024-09-04 09:38:43 -0700

    Remove force_ssl settings to fix healthchecks

--- config/prod.exs
+++ config/prod.exs
@@ -6,8 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :home, HomeWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
