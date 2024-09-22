155e59e38e6cd524b8ceb418019aefdfa4320d69
Matthew Lehner <matthew@mpl.io>
2024-09-22 09:59:00 -0700

    Add ADHD DTC resource link

--- lib/home_web/controllers/redirects.ex
+++ lib/home_web/controllers/redirects.ex
@@ -6,5 +6,13 @@ defmodule HomeWeb.Redirects do
   def call(%{request_path: "/writing/instrumenting-phoenix-with-prometheus"} = conn, _opts),
     do: conn |> redirect(to: ~p"/writing/collecting-phoenix-metrics-with-prometheus") |> halt()
 
+  def call(%{request_path: "/adhd-dtc"} = conn, _opts),
+    do:
+      conn
+      |> redirect(
+        external:
+          "https://reflect.site/g/mpl/disability-tax-credit-for-adhd/b58bed37edcb48d8b9b9c67f7863278a"
+      )
+
   def call(conn, _opts), do: conn
 end
